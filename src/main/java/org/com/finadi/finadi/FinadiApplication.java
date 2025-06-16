fc51f1ef3e61999787c7f81fcd18c695cf84decf
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-06-15 23:42:27 -0300

    first commit

---
+++ src/main/java/org/com/finadi/finadi/FinadiApplication.java
@@ -0,0 +1,13 @@
+package org.com.finadi.finadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinadiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinadiApplication.class, args);
+    }
+
+}
