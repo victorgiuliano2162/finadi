5b0a7bba5d97c345b82788f98ed08926532f4c94
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-06-15 23:46:47 -0300

    basic components for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/footer/footer.component';
+import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ToolBarComponent,
+    SidenavComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
