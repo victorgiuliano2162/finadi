3f9ae159e2c47092417e6daac9243ee62989b29b
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-06-25 13:26:33 -0300

    repository for Conta entity

---
+++ src/main/java/org/com/finadi/repositories/ContaRepository.java
@@ -0,0 +1,18 @@
+package org.com.finadi.repositories;
+
+import org.com.finadi.entities.Conta;
+import org.com.finadi.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ContaRepository extends JpaRepository<Conta, String> {
+
+  Optional<Conta> findByUsuarioId(String usuarioId);
+  Optional<List<Conta>> findContasByUsuarioId(String usuarioId);
+
+
+}
