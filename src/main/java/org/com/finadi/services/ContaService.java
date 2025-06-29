21646a720bd637fd6ae6c3e91ced2f481c015772
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-06-29 19:51:44 -0300

    new method to create conta

--- src/main/java/org/com/finadi/services/ContaService.java
+++ src/main/java/org/com/finadi/services/ContaService.java
@@ -1,9 +1,9 @@
 package org.com.finadi.services;
 
 
-import org.com.finadi.dto.ContaCriacaoDTO;
 import org.com.finadi.entities.Conta;
 import org.com.finadi.entities.Usuario;
+import org.com.finadi.exception.ResourceNotFoundException;
 import org.com.finadi.repositories.ContaRepository;
 import org.com.finadi.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ContaService {
@@ -20,38 +19,34 @@ public class ContaService {
   private ContaRepository contaRepository;
   @Autowired
   private UsuarioRepository usuarioRepository;
+  @Autowired
+  private UsuarioService usuarioService;
 
   public List<Conta> findAll() {
     return contaRepository.findAll();
   }
 
-  public Optional<Conta> findById(String id) {
-    return contaRepository.findById(id);
+  public Conta findById(String id) {
+
+    return contaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Nenhuma conta encontrada para o usuário"));
   }
 
-  public Optional<List<Conta>> getContas(String id) {
-    return contaRepository.findContasByUsuarioId(id);
+  public List<Conta> getContasByUserId(String id) {
+    return contaRepository.findContasByUsuarioId(id)
+      .orElseThrow(() -> new ResourceNotFoundException("Nenhuma conta encontrada para o usuário"));
   }
 
   public Conta salvarConta(Conta conta){
     return contaRepository.save(conta);
   }
 
+
   @Transactional
-  public Conta criarNovaConta(ContaCriacaoDTO contaDTO) {
-    String cpfUsuario = contaDTO.getUsuario().getCpf();
-    if (cpfUsuario == null || cpfUsuario.isBlank()) {
-      throw new IllegalArgumentException("O CPF do usuário é obrigatório.");
-    }
+  public Conta criarNovaConta(String userID, Conta conta) {
 
-    Usuario usuario = usuarioRepository.findByCpf(cpfUsuario);
-
-    Double saldoInicial = contaDTO.getSaldo();
-
-    Conta novaConta = new Conta(saldoInicial, usuario);
-
-
-    return contaRepository.save(novaConta);
+    Usuario userConta = usuarioService.findUserById(userID);
+    Conta contaCriada = new Conta(conta.getSaldo(), userConta, conta.getDataCriacao());
+    return contaRepository.save(contaCriada);
   }
 
   public Conta getContaById(String id) {
