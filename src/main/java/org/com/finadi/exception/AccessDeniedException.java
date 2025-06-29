6a1df134779251f3b296707bc96bcb2bc1269f19
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-06-29 19:49:12 -0300

    exception for handle access denied/user not found

---
+++ src/main/java/org/com/finadi/exception/AccessDeniedException.java
@@ -0,0 +1,18 @@
+package org.com.finadi.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class AccessDeniedException extends RuntimeException {
+
+
+  public AccessDeniedException(String mensagem) {
+    super(mensagem);
+  }
+
+  public AccessDeniedException(String mensagem, Throwable causa) {
+    super(mensagem, causa);
+  }
+}
