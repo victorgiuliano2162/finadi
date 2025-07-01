c6a5aa5ff6525d756086a2307593150c462bba17
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-06-30 21:21:29 -0300

    exception for resource not found

---
+++ src/main/java/org/com/finadi/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package org.com.finadi.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+
+  public ResourceNotFoundException(String mensagem) {
+    super(mensagem);
+  }
+
+  public ResourceNotFoundException(String mensagem, Throwable causa) {
+    super(mensagem, causa);
+  }
+}
