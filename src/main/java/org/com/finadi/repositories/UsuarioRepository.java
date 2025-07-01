49c889819c58082b2289fb76a41bebeae319c6ad
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-06-30 21:22:19 -0300

    repository for user

--- src/main/java/org/com/finadi/repositories/UsuarioRepository.java
+++ src/main/java/org/com/finadi/repositories/UsuarioRepository.java
@@ -1,31 +1,31 @@
-package org.com.finadi.dao;
+package org.com.finadi.repositories;
 
-import org.com.finadi.entities.User;
-import org.springframework.data.repository.CrudRepository;
+import org.com.finadi.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
 
   /**
    * Busca um produto pelo seu nome exato.
    * O Spring gera: "SELECT p FROM Produto p WHERE p.nome = ?1"
    */
-  Optional<User> findByName(String nome);
-  Optional<User> findByEmail(String email);
-
+  Optional<Usuario> findByName(String nome);
+  Optional<Usuario> findByEmail(String email);
+  Optional<Usuario> findByEmailContainingIgnoreCase(String email);
   /**
    * Busca todos os produtos de uma determinada categoria.
    * O Spring gera: "SELECT p FROM Produto p WHERE p.categoria = ?1"
    */
-
+  Usuario findByCpf(String cpf);
   /**
    * Busca produtos cujo nome contém o termo pesquisado, ignorando maiúsculas/minúsculas.
    * O Spring gera: "SELECT p FROM Produto p WHERE upper(p.nome) LIKE upper(?1)"
    * (O SQL exato pode variar com o dialeto do banco)
    */
-  List<User> findByNameContainingIgnoreCase(String termo);
+  List<Usuario> findByNameContainingIgnoreCase(String name);
 }
