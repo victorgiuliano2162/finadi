e68d5f97ae066b9ab9112e4026b7bfc9ed69a487
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-06-30 21:22:42 -0300

    user service with new logic to delete

--- src/main/java/org/com/finadi/services/UsuarioService.java
+++ src/main/java/org/com/finadi/services/UsuarioService.java
@@ -1,22 +1,32 @@
 package org.com.finadi.services;
 
+import org.com.finadi.entities.Conta;
 import org.com.finadi.entities.Usuario;
+import org.com.finadi.exception.ResourceNotFoundException;
+import org.com.finadi.exception.UserNotFoundException;
+import org.com.finadi.repositories.ContaRepository;
 import org.com.finadi.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.AccessDeniedException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UsuarioService {
 
   @Autowired
   private UsuarioRepository usuarioRepository;
+  @Autowired
+  private ContaRepository contaRepository;
 
   public Usuario saveUser(Usuario usuario) {
     return usuarioRepository.save(usuario);
+  }
+
+  public List<Usuario> saveAllUsers(List<Usuario> usuarios) {
+    return usuarioRepository.saveAll(usuarios);
   }
 
   public Usuario findByCpf(String cpf) {
@@ -29,8 +39,9 @@ public class UsuarioService {
     return usuarios;
   }
 
-  public Usuario getUserById(String id) {
-    return usuarioRepository.findById(id).orElseThrow();
+  public Usuario findUserById(String id) {
+    return usuarioRepository.findById(id)
+      .orElseThrow(() -> new UserNotFoundException(id));
   }
 
   public void deleteUserById(String id) {
@@ -38,7 +49,7 @@ public class UsuarioService {
   }
 
   public Usuario updateUser(Usuario usuario) {
-    getUserById(usuario.getId());
+    findUserById(usuario.getId());
     return usuarioRepository.save(usuario);
   }
 
@@ -46,5 +57,19 @@ public class UsuarioService {
     return usuarioRepository.findByEmail(email).orElseThrow();
   }
 
+  public void removerContaDoUsuario(String userId, String contaId) throws AccessDeniedException {
+    Usuario usuario = usuarioRepository.findById(userId)
+    .orElseThrow(() -> new ResourceNotFoundException("Usuário "+ userId+ " não encontrado"));
+
+    Conta conta = contaRepository.findById(contaId)
+      .orElseThrow(() -> new ResourceNotFoundException("Conta " +contaId+ " não encontrada"));
+
+    if (!conta.getUsuario().getId().equals(usuario.getId())) {
+      throw new AccessDeniedException("Conta não pertence ao usuário");
+    }
+
+    usuario.removerConta(conta);
+    usuarioRepository.save(usuario);
+  }
 
 }
