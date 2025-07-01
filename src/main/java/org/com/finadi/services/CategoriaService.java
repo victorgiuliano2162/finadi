4a32ddd4e8d0c121d9b92f1f6b5916dacbbbde2b
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-06-30 21:20:42 -0300

    service for category

--- src/main/java/org/com/finadi/services/CategoriaService.java
+++ src/main/java/org/com/finadi/services/CategoriaService.java
@@ -2,12 +2,11 @@ package org.com.finadi.services;
 
 
 import org.com.finadi.entities.Categoria;
-import org.com.finadi.entities.enums.TipoMovimentacao;
+import org.com.finadi.exception.ResourceNotFoundException;
 import org.com.finadi.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -17,20 +16,37 @@ public class CategoriaService {
   private CategoriaRepository categoriaRepository;
 
   public List<Categoria> findByNome(String nome) {
-    return categoriaRepository.findByNome(nome);
+    return categoriaRepository.findByNomeContainingIgnoreCase(nome);
+  }
+
+  public Categoria findOneByNome(String nome) {
+    return categoriaRepository.findByNomeIgnoreCaseAndUnaccent(nome)
+      .orElseThrow(() -> new ResourceNotFoundException("Categoria " + nome+ " não encontrada"));
+  }
+
+  public List<Categoria> getAll() {
+    return categoriaRepository.findAll();
   }
 
   public List<Categoria> findByUsuarioId(String usuarioId) {
     return categoriaRepository.findByUsuarioId(usuarioId);
   }
 
-  public void deleteByNome(Categoria categoria) {
-    categoriaRepository.deleteById(categoria.getId());
+  public Categoria findById(String id) {
+    return categoriaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Categoria com o " + id + " não encontrada"));
   }
 
-  public void deleteByUsuarioId(String usuarioId) {
-    categoriaRepository.deleteByUsuarioId(usuarioId);
+  public void deleteById(String id) {
+    Categoria categoria = categoriaRepository
+      .findById(id)
+      .orElseThrow(() -> new ResourceNotFoundException("Categoria com o " + id + " não encontrada"));
+    categoriaRepository.deleteById(id);
   }
+
+  //TODO: revisar método, como está apagaria todas as categorias de um user
+//  public void deleteByUsuarioId(String usuarioId) {
+//    categoriaRepository.deleteByUsuarioId(usuarioId);
+//  }
 
   public Categoria salvarCategoria(Categoria categoria) {
     return categoriaRepository.save(categoria);
