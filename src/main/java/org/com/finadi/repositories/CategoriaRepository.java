c036f635becd0a42654cbd5d09f019c2b21f843b
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-06-30 21:20:32 -0300

    method for seach catregory without depending of accent and words case

--- src/main/java/org/com/finadi/repositories/CategoriaRepository.java
+++ src/main/java/org/com/finadi/repositories/CategoriaRepository.java
@@ -5,20 +5,36 @@ import org.com.finadi.entities.Categoria;
 import org.com.finadi.entities.Movimentacao;
 import org.com.finadi.entities.enums.TipoMovimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, String> {
 
-  List<Categoria> findByNome(String nome);
+  List<Categoria> findByNomeContainingIgnoreCase(String nome);
 
   List<Categoria> findByUsuarioId(String usuarioId);
 
   void deleteByUsuarioId(String usuarioId);
 
 
-  }
+// No seu CategoriaRepository.java
+
+  @Query(
+    // Dizemos explicitamente para usar a função unaccent do schema 'public'
+    value = "SELECT * FROM categoria " +
+      "WHERE public.unaccent(nome::text) ILIKE public.unaccent(concat('%', :nome, '%')) " +
+      "LIMIT 1",
+    nativeQuery = true
+  )
+  Optional<Categoria> findByNomeIgnoreCaseAndUnaccent(@Param("nome") String nome);
+
+
+
+}
