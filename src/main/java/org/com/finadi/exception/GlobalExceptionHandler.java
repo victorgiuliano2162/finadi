ac958496fddd37c28edfcf88ca34517a88a57c75
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-06-30 21:21:09 -0300

    global handler exception

---
+++ src/main/java/org/com/finadi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package org.com.finadi.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(UserNotFoundException.class)
+  public ResponseEntity<String> handleNotFoundException(UserNotFoundException ex) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+  }
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<Map<String, String>> handleNotFound(ResourceNotFoundException ex) {
+    Map<String, String> erro = new HashMap<>();
+    erro.put("erro", ex.getMessage());
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+  }
+
+  @ExceptionHandler(AccessDeniedException.class)
+  public ResponseEntity<Map<String, String>> handleAccessDenied(AccessDeniedException ex) {
+    Map<String, String> erro = new HashMap<>();
+    erro.put("erro", ex.getMessage());
+    return ResponseEntity.status(HttpStatus.FORBIDDEN).body(erro);
+  }
+
+}
