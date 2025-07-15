f0fb59283c352a309a276d316f61ad0c99125d4d
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-07-15 00:00:05 -0300

    user service

---
+++ src/app/service/shared/user-service.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Login} from "../../models/login.model";
+import {Observable} from "rxjs";
+import { User } from 'src/app/models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+
+  constructor(private httpCliente: HttpClient) { }
+
+  api: string = "http://localhost:8080/api/user";
+
+  public registerUser(user: User): Observable<User> {
+    return this.httpCliente.post<User>(`${this.api}`, user);
+  }
+
+  public getUserByCpf(cpf: string): Observable<User> {
+    return this.httpCliente.get<User>(`${this.api}/cpf/${cpf}`);
+  }
+
+  public getByCpf(cpf: string): Observable<User> {
+    const cpfLimpo = cpf.replace(/\D/g, '');
+    
+    // Cria os parâmetros de forma segura
+    const params = new HttpParams().set('cpf', cpfLimpo);
+    
+    // Passa os parâmetros como uma opção para a requisição GET
+    return this.httpCliente.get<User>(this.api, { params: params });
+  }
+  
+}
