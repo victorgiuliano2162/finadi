eb13fac3b7a5bc89664d38365d3953daedbe707d
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-07-22 08:12:09 -0300

    new user logged logic

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,26 +1,27 @@
-import {Component, ViewChild} from '@angular/core';
-import {MatDrawer} from "@angular/material/sidenav";
-import {Subscription} from "rxjs";
-import {UiService} from "../../service/shared/ui.service";
+import { Component, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { Observable, Subscription } from 'rxjs';
+import { UiService } from '../../service/shared/ui.service';
+import { User } from 'src/app/models/usuario.model';
+import { AuthService } from 'src/app/service/shared/auth.service';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.css'],
 })
 export class SidenavComponent {
-
+  currentUser$: Observable<User | null>;
   @ViewChild('drawer') private drawer!: MatDrawer;
   private drawerSubscription!: Subscription;
   cont = 0;
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private authService: AuthService) {
     this.cont++;
-    console.log(this.cont)
+    this.currentUser$ = this.authService.currentUser$;
   }
 
-
   public show() {
-    if(this.cont = 1) {
+    if ((this.cont = 1)) {
       this.toggleDrawer();
     }
   }
@@ -44,4 +45,7 @@ export class SidenavComponent {
     }
   }
 
+  logout() {
+    this.authService.logout();
+  }
 }
--- src/app/components/tool-bar/tool-bar.component.ts
+++ src/app/components/tool-bar/tool-bar.component.ts
@@ -1,18 +1,26 @@
-
 import { Component } from '@angular/core';
-import { UiService } from "../../service/shared/ui.service";
+import { UiService } from '../../service/shared/ui.service';
+import { AuthService } from 'src/app/service/shared/auth.service';
+import { User } from 'src/app/models/usuario.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tool-bar',
   templateUrl: './tool-bar.component.html',
-  styleUrls: ['./tool-bar.component.css']
+  styleUrls: ['./tool-bar.component.css'],
 })
 export class ToolBarComponent {
+  constructor(private uiService: UiService, private authService: AuthService) {
+    this.currentUser$ = this.authService.currentUser$;
+  }
 
-  constructor(private uiService: UiService) { }
-
+  currentUser$: Observable<User | null>;
 
   onToggleSidenav() {
     this.uiService.toggleSidenav();
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
