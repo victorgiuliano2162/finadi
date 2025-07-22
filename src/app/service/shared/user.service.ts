37e2cbdcde6f03828a0aa6fc0a4eadc9acec1ee7
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-07-22 08:13:30 -0300

    new user service

--- src/app/service/shared/user.service.ts
+++ src/app/service/shared/user.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {Login} from "../../models/login.model";
-import {Observable} from "rxjs";
-import { User } from 'src/app/models/user.model';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/usuario.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class UserServiceService {
+export class UserService {
+  constructor(private httpCliente: HttpClient) {}
 
-  constructor(private httpCliente: HttpClient) { }
-
-  api: string = "http://localhost:8080/api/user";
+  api: string = 'http://localhost:8080/api/user';
 
   public registerUser(user: User): Observable<User> {
     return this.httpCliente.post<User>(`${this.api}`, user);
@@ -21,14 +19,17 @@ export class UserServiceService {
     return this.httpCliente.get<User>(`${this.api}/cpf/${cpf}`);
   }
 
+  public getUserByEmail(user: User): Observable<User> {
+    return this.httpCliente.post<User>(`${this.api}/login`, user);
+  }
+
   public getByCpf(cpf: string): Observable<User> {
     const cpfLimpo = cpf.replace(/\D/g, '');
-    
+
     // Cria os parâmetros de forma segura
     const params = new HttpParams().set('cpf', cpfLimpo);
-    
+
     // Passa os parâmetros como uma opção para a requisição GET
     return this.httpCliente.get<User>(this.api, { params: params });
   }
-  
 }
