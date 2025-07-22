1cb681f8848f5b8fbfd074c3c5ab80d93099f34e
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-07-22 08:14:11 -0300

    method to debug

--- src/main/java/org/com/finadi/controllers/UsuarioController.java
+++ src/main/java/org/com/finadi/controllers/UsuarioController.java
@@ -6,10 +6,13 @@ import org.com.finadi.entities.Usuario;
 import org.com.finadi.services.ContaService;
 import org.com.finadi.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -21,12 +24,26 @@ public class UsuarioController {
   @Autowired
   private ContaService contaService;
 
+  @GetMapping("/getAll")
+  public ResponseEntity<List<Usuario>> getAllUsers() {
+    List<Usuario> usuarios = usuarioService.findAll();
+    return ResponseEntity.ok(usuarios);
+  }
 
+  @PostMapping("/login")
+  public ResponseEntity<Usuario> login(@RequestBody Usuario usuario ) {
+    Usuario user = usuarioService.findByEmail(usuario.getEmail());
+    if (user.getPassword().equals(usuario.getPassword())) {
+      return ResponseEntity.ok(user);
+    }
+    return ResponseEntity.notFound().build();
+  }
 
   @PostMapping()
-  public ResponseEntity<Usuario> createUser(@RequestBody @Valid Usuario usuario) {
-    Usuario savedUser = usuarioService.saveUser(usuario);
-    return ResponseEntity.ok(savedUser);
+  public ResponseEntity<Usuario> createUser(@RequestBody Usuario usuario) {
+    Usuario userToSave = usuarioService.saveUser(usuario);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userToSave);
+
   }
 
 
@@ -35,6 +52,13 @@ public class UsuarioController {
     Usuario user = usuarioService.findUserById(id);
     return ResponseEntity.ok(user);
   }
+
+  @GetMapping
+  public ResponseEntity<Usuario> getUserById(@RequestParam(name = "cpf") String cpf) {
+    Usuario user = usuarioService.findByCpf(cpf);
+    return ResponseEntity.ok(user);
+  }
+
 
 
   @DeleteMapping("/{id}")
