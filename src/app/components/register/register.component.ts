50c61fa0aca542f91f7bd26942545627d4c4bfc6
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-07-22 08:11:37 -0300

    register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,54 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../../service/shared/user.service';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/models/usuario.model';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
-  mainForm!: FormGroup;
+export class RegisterComponent {
+  constructor(private userService: UserService, private router: Router) {}
 
-  constructor(private _formBuilder: FormBuilder) {}
+  subscribeUserFrom: User = {
+    cpf: '',
+    name: '',
+    email: '',
+    password: '',
+  };
 
-  ngOnInit() {
-    // Construímos a estrutura aninhada do formulário para corresponder ao HTML
-    this.mainForm = this._formBuilder.group({
-      // 1. FormGroup para o primeiro passo
-      dadosPessoais: this._formBuilder.group({
-        // Alterado para corresponder ao formControlName="firstCtrl" do HTML
-        firstCtrl: ['', Validators.required]
-      }),
-      // 2. FormGroup para o segundo passo
-      informacoesContato: this._formBuilder.group({
-        // Alterado para corresponder ao formControlName="secondCtrl" do HTML
-        secondCtrl: ['', Validators.required]
-      })
-      // O grupo 'endereco' foi removido pois não é usado no seu HTML.
-    });
-  }
+  public submitUserForm(subscribeUserFrom: NgForm) {
+    console.log('Submit chamado');
 
-  // Getter para facilitar o acesso ao FormGroup do primeiro passo
-  get dadosPessoais(): FormGroup {
-    return this.mainForm.get('dadosPessoais') as FormGroup;
-  }
+    if (subscribeUserFrom.valid) {
+      console.log('Dados: ' + this.subscribeUserFrom.email);
 
-  // Getter para facilitar o acesso ao FormGroup do segundo passo
-  get informacoesContato(): FormGroup {
-    return this.mainForm.get('informacoesContato') as FormGroup;
+      this.userService.registerUser(this.subscribeUserFrom).subscribe({
+        next: (res: User) => {
+          console.log('Usuário registrado com sucesso');
+          console.log(res);
+          setTimeout(() => {}, 2000);
+          this.router.navigate(['/dashboard']);
+        },
+        error: (err: any) => {
+          alert(err.error.erro);
+          console.log();
+        },
+      });
+    } else {
+      console.log('Formulário inválido');
+      alert('Impossível realizar operação');
+    }
   }
 
   // Função para ser chamada na submissão do formulário
-  onSubmit(): void {
-    if (this.mainForm.valid) {
-      console.log("Formulário enviado com sucesso!");
-      console.log(this.mainForm.value);
-      // Exemplo de como acessar os valores:
-      // const nome = this.mainForm.value.dadosPessoais.firstCtrl;
-      // const endereco = this.mainForm.value.informacoesContato.secondCtrl;
-      alert('Formulário enviado! Verifique o console.');
-    } else {
-      alert('Formulário inválido. Por favor, preencha todos os campos.');
-    }
-  }
 }
