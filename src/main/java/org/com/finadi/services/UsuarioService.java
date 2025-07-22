a24fb79a6a9321445e9b569980b7e081cf16de9b
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-07-22 08:15:00 -0300

    .

--- src/main/java/org/com/finadi/services/UsuarioService.java
+++ src/main/java/org/com/finadi/services/UsuarioService.java
@@ -2,8 +2,8 @@ package org.com.finadi.services;
 
 import org.com.finadi.entities.Conta;
 import org.com.finadi.entities.Usuario;
+import org.com.finadi.exception.ParameterinvalidException;
 import org.com.finadi.exception.ResourceNotFoundException;
-import org.com.finadi.exception.UserNotFoundException;
 import org.com.finadi.repositories.ContaRepository;
 import org.com.finadi.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +21,15 @@ public class UsuarioService {
   @Autowired
   private ContaRepository contaRepository;
 
+  public List<Usuario> findAll() {
+    return usuarioRepository.findAll();
+  }
+
   public Usuario saveUser(Usuario usuario) {
-    return usuarioRepository.save(usuario);
+    if (usuarioRepository.findByEmail(usuario.getEmail()).isEmpty()) {
+      return usuarioRepository.save(usuario);
+    }
+    throw new ParameterinvalidException("Email já cadastrado");
   }
 
   public List<Usuario> saveAllUsers(List<Usuario> usuarios) {
@@ -33,29 +40,31 @@ public class UsuarioService {
     return usuarioRepository.findByCpf(cpf);
   }
 
+  public Usuario findByEmail(String email) {
+    String errorMessage= "User with email: "+ email +" not found";
+    return usuarioRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException(errorMessage));
+  }
+
   public List<Usuario> getUsers() {
     List<Usuario> usuarios = new ArrayList<>();
     usuarioRepository.findAll().forEach(usuarios::add);
     return usuarios;
   }
 
-  public Usuario findUserById(String id) {
+  public Usuario findUserById(String id)   {
     return usuarioRepository.findById(id)
-      .orElseThrow(() -> new UserNotFoundException(id));
+      .orElseThrow(() -> new ResourceNotFoundException(id));
   }
 
   public void deleteUserById(String id) {
     usuarioRepository.deleteById(id);
   }
 
-  public Usuario updateUser(Usuario usuario) {
+  public Usuario updateUser(Usuario usuario)  {
     findUserById(usuario.getId());
     return usuarioRepository.save(usuario);
   }
 
-  public Usuario findByEmail(String email) {
-    return usuarioRepository.findByEmail(email).orElseThrow();
-  }
 
   public void removerContaDoUsuario(String userId, String contaId) throws AccessDeniedException {
     Usuario usuario = usuarioRepository.findById(userId)
