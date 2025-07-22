196ae5bb70eecea59ad63d5332ab4de1d13f1eb5
victorgiuliano2162 <vitor.giuliano@gmail.com>
2025-07-22 08:11:13 -0300

    new logic for user login

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,51 +1,43 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Login } from 'src/app/models/login.model';
-import {LoginService} from "../../service/login.service";
-import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/shared/auth.service';
+import { User } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
-
 export class LoginPageComponent {
-  constructor(private loginService: LoginService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  loginForm: Login = {
-    email: "",
-    password: "",
+  loginForm: User = {
+    cpf: '',
+    name: '',
+    email: '',
+    password: '',
   };
 
   public submitLogin(loginForm: NgForm) {
-    console.log("Submit chamado");
+    console.log('Submit chamado');
     console.log(loginForm.value);
 
-    if(loginForm.valid) {
-      console.log("Dados: "+ this.loginForm.email);
-      console.log("Dados: "+ this.loginForm.password);
+    if (loginForm.valid) {
+      console.log('Dados: ' + this.loginForm.email);
+      console.log('Dados: ' + this.loginForm.password);
 
-      this.loginService.testLogin(this.loginForm).subscribe({
-        next: (res: Login) => {
-          console.log("Enviou");
-          console.log(res);
-        },
-        error: (err: HttpErrorResponse) => {
-          console.log("Chamou o erro");
-          console.log(err);
-        }
-      });
+      this.authService.login(this.loginForm);
     } else {
-      console.log("Formulário inválido");
+      console.log('Formulário inválido');
+      alert('Formulário inválido');
     }
   }
-
 }
-  // public getLogin() {
-  //   this.loginService.testGet().subscribe({
-  //     next: (res: Login) => {
-  //       console.log(res);
-  //     }
-  //   })
-  // }
+// public getLogin() {
+//   this.loginService.testGet().subscribe({
+//     next: (res: Login) => {
+//       console.log(res);
+//     }
+//   })
+// }
